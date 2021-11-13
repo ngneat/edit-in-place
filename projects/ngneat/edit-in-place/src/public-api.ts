755d07537cd6934504cd15405e27f115899ac590
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-11-13 09:45:13 +0100

    fix(lib): export directives/components due to Angular 13 migration

--- projects/ngneat/edit-in-place/src/public-api.ts
+++ projects/ngneat/edit-in-place/src/public-api.ts
@@ -4,3 +4,16 @@
 
 export * from './lib/editable.module';
 export * from './lib/editable.config';
+
+export * from './lib/directives/editable-group-edit.directive';
+export * from './lib/directives/editable-group-save.directive';
+export * from './lib/directives/editable-group-cancel.directive';
+export * from './lib/directives/editable-group.directive';
+export * from './lib/directives/editable-on-escape.directive';
+export * from './lib/directives/editable-on-enter.directive';
+export * from './lib/directives/editable-focus.directive';
+export * from './lib/directives/editable-cancel.directive';
+export * from './lib/directives/editable-save.directive';
+export * from './lib/editable.component';
+export * from './lib/directives/edit-mode.directive';
+export * from './lib/directives/view-mode.directive';
